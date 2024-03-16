fcbc9414955072d22dd9628d3b77c26f4009f31c
allan <allanmangune@hotmail.com>
2024-03-15 21:09:22 -0600

    Added implementing code to the AddProductCommandHandler.

--- CleanArchitectureECommerce.Application/Products/Commands/AddProductCommandHandler.cs
+++ CleanArchitectureECommerce.Application/Products/Commands/AddProductCommandHandler.cs
@@ -1,11 +1,34 @@
 ﻿using System;
-namespace CleanArchitectureECommerce.Application.Products
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitectureECommerce.Core.Entities;
+using CleanArchitectureECommerce.Core.Interfaces;
+using CleanArchitecture.Application.Products.Commands;
+
+namespace CleanArchitectureECommerce.Application.Products.Commands
 {
-	public class AddProductCommandHandler
-	{
-		public AddProductCommandHandler()
-		{
-		}
-	}
+    public class AddProductCommandHandler : IRequestHandler<AddProductCommand, int>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public AddProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<int> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Price = request.Price
+            };
+
+            await _productRepository.AddAsync(product);
+            return product.Id; 
+        }
+    }
 }
+
 
